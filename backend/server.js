c02b5362b78f0e0cfc4e23fa4ff767e2215debe4
Azan Meer <azanmeer67@gmail.com>
2026-04-24 23:34:28 +0500

    fixed cors issue

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,12 @@ const studentRoutes = require("./routes/studentRoutes");
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 app.use(express.json());
 
 // Connect MongoDB
