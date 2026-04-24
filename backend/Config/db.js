d99200c8c4868ba1465cda7c383252ee3ff75923
Azan Meer <azanmeer67@gmail.com>
2026-04-24 18:13:02 +0500

    fix backend db import

--- backend/Config/db.js
+++ backend/Config/db.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Azanmeer123", // 👈 put your MySQL password here
-  database: "student_db"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 db.connect((err) => {
